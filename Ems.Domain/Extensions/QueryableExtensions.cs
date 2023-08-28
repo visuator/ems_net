78f757e8117d8632373159e852cf99061c5c51c4
n.korabelnikov <n.korabelnikov@isoit.ru>
2023-08-28 21:37:43 +0300

    fix

--- Ems.Domain/Extensions/QueryableExtensions.cs
+++ Ems.Domain/Extensions/QueryableExtensions.cs
@@ -22,11 +22,9 @@ public static class QueryableExtensions
     public static IQueryable<T> ODataMapFromRoles<T>(this IQueryable<T> query, List<Role> roles)
     {
         var dt1 = query.Expression as MethodCallExpression;
-        var dt2 = dt1!.Arguments.OfType<UnaryExpression>().First();
-        var dt3 = dt2.Operand as LambdaExpression;
+        var dt2 = dt1!.Arguments[1] as UnaryExpression;
+        var dt3 = dt2!.Operand as LambdaExpression;
         var dt4 = dt3!.Body as MemberInitExpression;
-
-        var method = dt1.Method;
 
         MemberInitExpression FilteredBindings(List<Role> list, MemberInitExpression? memberInitExpression)
         {
@@ -44,24 +42,23 @@ public static class QueryableExtensions
                 if (fb is not MemberAssignment fba) continue;
                 var expr = fba.Expression as MethodCallExpression;
                 if(expr is null) continue;
-                    
                 var args = expr.Arguments[0] as MethodCallExpression;
-                if (args.Method.Name == "Select")
-                {
-                    var l1 = args.Arguments[1] as LambdaExpression;
-                    var memInit = l1.Body as MemberInitExpression;
-                    var t = FilteredBindings(roles, memInit);
-                    memberBindings = memberBindings.Where(x => x != fb).ToList();
-                    memberBindings.Add(Expression.Bind(fb.Member,
-                        Expression.Call(expr.Method, Expression.Call(args.Method, args.Arguments[0], Expression.Lambda(t, false, l1.Parameters)))));
-                }
+                if (args is null) continue;
+                if (args.Method.Name != "Select") continue;
+                
+                var l1 = args.Arguments[1] as LambdaExpression;
+                var memInit = l1.Body as MemberInitExpression;
+                var t = FilteredBindings(roles, memInit);
+                memberBindings = memberBindings.Where(x => x != fb).ToList();
+                memberBindings.Add(Expression.Bind(fb.Member,
+                    Expression.Call(expr.Method, Expression.Call(args.Method, args.Arguments[0], Expression.Lambda(t, false, l1.Parameters)))));
             }
 
             return Expression.MemberInit(memberInitExpression.NewExpression, memberBindings);
         }
         
         var lambda = Expression.Lambda(FilteredBindings(roles, dt4), false, dt3.Parameters);
-        var call = Expression.Call(null, method, dt1.Arguments[0], lambda);
+        var call = Expression.Call(null, dt1.Method, dt1.Arguments[0], lambda);
 
         return query.Provider.CreateQuery<T>(call);
     }
